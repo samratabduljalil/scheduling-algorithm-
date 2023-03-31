8266f36510c9d43fe747ac473d71c936762c7c80
Samrat Abdul Jalil <95965521+Samrat-Abdul-Jalil@users.noreply.github.com>
2023-04-01 00:47:54 +0600

    Add files via upload

--- SRTF && priority scheduling.cpp
+++ SRTF && priority scheduling.cpp
@@ -16,7 +16,7 @@ struct comp{
 
 bool operator()(process const& p1,process const& p2){
 
-return p1.ReT <p2.ReT;
+return p1.ReT > p2.ReT;
 
 
 }
@@ -24,19 +24,38 @@ return p1.ReT <p2.ReT;
 
 
 };
+ int compTime=0;
 
-
-void SRTF(process a[],int n,int c){
+void SRTF(process a[],int n){
 
 priority_queue<process,vector<process>,comp> PQ;
 
-PQ.push(a[0]);
+bool h = true;
+
+while(h){
+int y = 0;
+for(int i=0;i<n;i++){
+if(a[i].AT <= y){
+PQ.push(a[i]);
+
+
+}
+}
+
+
+
+
+
+}
+
+
+PQ.push(a[1]);
  process d;
 d = PQ.top();
 
  printf("SRTF AT = %d\n",d.AT);
    a[0].AT=10;
-
+compTime++;
 
 }
 
@@ -51,7 +70,7 @@ void Priority(process a){
 
 int main(){
     int p;
-    int compTime=0;
+
     struct process pro[100];
 scanf("%d",&p);
 for(int i=0;i<p;i++){
@@ -73,7 +92,11 @@ for(int i=0;i<p;i++){
 
 
 }
-SRTF(pro,p,compTime);
+printf("compTime = %d\n",compTime);
+SRTF(pro,p);
  printf("after SRTF AT = %d\n",pro[0].AT);
+
+ printf("BT compTime = %d\n",compTime);
+
 return 0;
 }
